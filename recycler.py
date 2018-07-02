b9236c03c510492a66e6a357bbba56d92fdcf614
James Annis <annis@fnal.gov>
2018-07-02 14:02:20 -0500

    fixing code format

--- recycler.py
+++ recycler.py
@@ -859,8 +859,8 @@ if __name__ == "__main__":
                 #print os.path.join(trigger_path, trigger_id,'default_skymap.txt')
                 #print os.path.join(trigger_path, trigger_id,'default_skymap.txt').read()
                 skymap_filename = os.path.join(trigger_path, trigger_id,
-                                               open(os.path.join(trigger_path, trigger_id,
-                                                                 'default_skymap.txt'),'r').read())
+                    open(os.path.join(trigger_path, trigger_id,
+                   'default_skymap.txt'),'r').read())
             except:
                badtriggers = open('badtriggers.txt', 'a')
                badtriggers.write(trigger_id + '\n')
