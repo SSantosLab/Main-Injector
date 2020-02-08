34e42c2d04488b6a52df87adb0789b09e1331bc2
James Annis <annis@fnal.gov>
2020-02-08 02:43:02 -0600

    Stamped out an evil bug when hexes have the same probabilities. Just pick one

--- python/obsSlots.py
+++ python/obsSlots.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import warnings
 
 # ========== do simple calculations on how to divide the night
 #
@@ -139,7 +140,7 @@ def observing(sim, nslots, data_dir,
         raHexen, decHexen, idHexen, hexVal, rank, mjd, slotNum = \
            loadHexalatedProbabilities( sim, map_i, data_dir)
         islot = i*np.ones(raHexen.size)
-        print "\t", map_i, "map size= {};".format(raHexen.size), 
+        print "\t", map_i, "map size= {:10d};".format(raHexen.size), 
 
         impossible = 1e-5
         impossible = 1e-7
@@ -224,7 +225,7 @@ def observing(sim, nslots, data_dir,
             if verbose >= 1: 
                 print "n slots =", len(observingSlots)," == 0?"
                 print "sumHexes = ", sumHexes, "==? 0"
-            print "\tnumber of hexes observed = ", sumObs
+            print "\tnumber of hexes possible to observe = ", sumObs
             print "\t======================================== "
             return slotsObserving 
 
@@ -251,7 +252,7 @@ def observingStats( slotsObserving, mapZero=0, do_nslots=-1, start_slot=-1 ) :
             print "  sum prob= {:7.4f} %".format( 100*slotsObserving[i,"prob"].sum())
     ra,dec,id,prob,mjd,slotNum,islot = slotsObservingToNpArrays(slotsObserving) 
 
-    print "\t observingStats:  ",
+    print "\tobservingStats:  ",
     print "observable prob_tot = {:.1f}%".format(100.*prob.sum())
     print "\t   (from the prob in each slot, summed)"
     return ra,dec,id,prob,mjd,slotNum,islot
@@ -267,10 +268,10 @@ def observingStatsFromRaDecFile( trigger_id, data_dir, slotsObserving, mapZero=0
         if ix.size > 0 :
             slot_sum_prob = 100*prob[ix].sum() 
             if slot_sum_prob > 1e-7 :
-                print "\t ",i+mapZero, 
+                print "\t",i+mapZero, 
                 print "n hexes= {}".format( ix.size ),
                 print "  sum prob= {:7.4f} %".format( slot_sum_prob )
-    print "\t observingStats:  ",
+    print "\tobservingStats:  ",
     print "observable prob_tot = {:.1f}%".format(100.*prob.sum())
     print "\t   (from the prob in each slot, summed)"
     return ra,dec,id,prob,mjd,slotNum,dist
@@ -349,7 +350,7 @@ def findMaxProbOfAllHexes(hexData, slotsObserving, observingSlots, n="", verbose
         if verbose >= 4: print n,i, maxProb, ">?", newProb, "     n=",hexVal.size
         if newProb > maxProb :
             if verbose >= 1: print n,"==== new max", i, "       ",newProb , ">", maxProb
-            ix = hexVal == newProb
+            ix = np.where(hexVal == newProb)
             maxRa     = hexRa[ix]
             maxDec    = hexDec[ix]
             maxId     = hexId[ix]
@@ -363,6 +364,9 @@ def findMaxProbOfAllHexes(hexData, slotsObserving, observingSlots, n="", verbose
         maxRa, maxDec, maxId, maxVal, maxMjd, maxSlot, islot = \
             -1,-1,-1,-1,-1,-1,-1
         #raise Exception("no max probability found")
+    if len(maxRa) > 1 :
+        maxRa, maxDec, maxId, maxVal, maxMjd, maxSlot, islot = \
+        maxRa[0], maxDec[0], maxId[0], maxVal[0], maxMjd[0], maxSlot[0], islot
     return maxRa, maxDec, maxId, maxVal, maxMjd, maxSlot, islot
 
 # we've found a hex,slot that can be observed so add it the the observing lists
@@ -400,6 +404,7 @@ def deleteHexFromSlot (hexData, slot, maxProb) :
 # the hex,slot has made it onto an observing list, so remove the hex
 # from all hex lists ( rm hex,*)
 def deleteHexFromAllSlots (hexData, slotsObserving, observingSlots, maxRa, maxDec, verbose=0, n="") :
+    warnings.filterwarnings("ignore")
     do_nslots  = slotsObserving["do_nslots"]
     start_slot = slotsObserving["start_slot"]
     nslots     = slotsObserving["nslots"]
@@ -418,7 +423,7 @@ def deleteHexFromAllSlots (hexData, slotsObserving, observingSlots, maxRa, maxDe
         hexMjd = data[4]
         hexSlotNum = data[5]
         hexIslot   = data[6]
-        ix = np.nonzero((hexRa == maxRa) & (hexDec == maxDec))
+        ix, = np.where((hexRa == maxRa) & (hexDec == maxDec))
         if verbose >=4 : print ix, hexRa, maxRa
         if verbose >=2 : 
             ixs = np.shape(ix)[1]
