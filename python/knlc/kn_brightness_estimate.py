d02491bff2e50811a751ea5ad6586a9167b4b2c1
Robert Morgan <39347211+rmorgan10@users.noreply.github.com>
2020-02-18 21:30:55 -0600

    expand to 9.5 days

--- python/knlc/kn_brightness_estimate.py
+++ python/knlc/kn_brightness_estimate.py
@@ -23,8 +23,8 @@ class KNCalc():
         self.distance_err = distance_err
         
         # Convert time_delay from hours to days
-        if float(time_delay) > 170.0:
-            print("Currently, only time delays less than 170 hours (7.1 days) post merger are supported")
+        if float(time_delay) > 228.0:
+            print("Currently, only time delays less than 228 hours (9.5 days) post merger are supported")
             sys.exit()
         self.delta_mjd = round(float(time_delay) / 24.0, 1)
         
@@ -39,6 +39,8 @@ class KNCalc():
             df = pd.read_csv(knlc_dir+'data/grouped_photometry_2.csv')
         elif self.delta_mjd < 7.1:
             df = pd.read_csv(knlc_dir+'data/grouped_photometry_3.csv')
+        elif self.delta_mjd < 9.5:
+            df = pd.read_csv(knlc_dir+'data/grouped_photometry_4.csv')
         df['ZMEAN'] = np.mean(df[['ZMIN', 'ZMAX']].values, axis=1)
 
         # Mean distance calculation 
