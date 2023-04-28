6d903f57190f8d6d12641f924ae8f962ca365273
Elise Kesler <eliseke@des70.fnal.gov>
2023-04-28 17:05:23 -0500

    added back in source, changed np.int/float to int64 and float64, fixed prints

--- python/simplicity.py
+++ python/simplicity.py
@@ -43,9 +43,9 @@ def calc(ra, dec, prob, expTime, filter, mjd, best=False, camera="decam"):
 
     # find night statistics
     night, sunset, sunrise = mags.findNightDuration(mjd, camera)
-    night = np.float(night)*24.
-    sunset = np.float(sunset)
-    sunrise = np.float(sunrise)
+    night = np.float64(night)*24.
+    sunset = np.float64(sunset)
+    sunrise = np.float64(sunrise)
 
     # will work every hour from sunset to sunrise
     mjd_list = np.arange(sunset, sunrise+1./24., 1./24.)
@@ -62,7 +62,7 @@ def calc(ra, dec, prob, expTime, filter, mjd, best=False, camera="decam"):
         limit_mag.append(obs.maglim)
         best_limit_mag.append(obs.maglim[best_ix])
         moon_sep.append(obs.moonSep[0]*360./2/np.pi)
-        moon_phase.append(obs.moonPhase)
+        Moon_phase.append(obs.moonPhase)
     limit_mag = np.vstack(limit_mag)
     ix = limit_mag < 0; limit_mag[ix] = 0
     moon_sep = np.array(moon_sep)
@@ -73,29 +73,29 @@ def calc(ra, dec, prob, expTime, filter, mjd, best=False, camera="decam"):
     bins = np.arange(21,25.5,0.5)
 
     # now print the answers
-    print("{:15s}  {:10s}".format("MJD".rjust(15),"best".rjust(10)))
-    print(" {:10s}".format("moon sep".rjust(10)))
-    print(" {:10s}".format("moon phase".rjust(10)))
-    print("      hexes w/ limiting mag in "),
+    print("{:15s}  {:10s}".format("MJD".rjust(15),"best".rjust(10)), end='')
+    print(" {:10s}".format("moon sep".rjust(10)), end='')
+    print(" {:10s}".format("moon phase".rjust(10)), end='')
+    print("      hexes w/ limiting mag in ", end='')
     print("")
-    print("{:15s}  {:10s}".format("days".rjust(15),"hex".rjust(10))),
-    print(" {:10s}".format("degrees".rjust(10)))
-    print("  {:10s}".format("% full ".rjust(10)))
-    for i in range(bins.size-1) : print("{:4.1f}-".format(bins[i]))
+    print("{:15s}  {:10s}".format("days".rjust(15),"hex".rjust(10)), end='')
+    print(" {:10s}".format("degrees".rjust(10)), end='')
+    print("  {:10s}".format("% full ".rjust(10)), end='')
+    for i in range(bins.size-1) : print("{:4.1f}-".format(bins[i]), end='')
     print("{:4.1f}".format(bins[i]))
     print("")
     for i in range(0,mjd_list.size) :
-        print("{:15.4f} ".format(mjd_list[i]))
-        print("{:10.2f} ".format(best_limit_mag[i]))
-        print("{:10.2f} ".format(moon_sep[i]))
-        print("{:10.0f}  ".format(moon_phase[i]))
+        print("{:15.4f} ".format(mjd_list[i]), end='')
+        print("{:10.2f} ".format(best_limit_mag[i]), end='')
+        print("{:10.2f} ".format(moon_sep[i]), end='')
+        print("{:10.0f}  ".format(moon_phase[i]), end='')
         counts = np.histogram(limit_mag[i], bins)
         #print "\n counts ", np.shape(counts), counts[0], counts[1]
         for j in range(bins.size-1) : 
             if counts[0][j] != 0 :
-                print("{:3d}  ".format(counts[0][j])),
+                print("{:3d}  ".format(counts[0][j]), end='')
             else :
-                print("     "),
+                print("     ", end='')
         print("")
     return obs
 
