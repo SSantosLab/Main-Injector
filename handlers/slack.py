bfa80d6496c749f8144d3f97f45c8829c314e4fd
Sean MacBride <sean.p.macbride@gmail.com>
2024-03-19 12:42:00 -0700

    Update slack.py

--- handlers/slack.py
+++ handlers/slack.py
@@ -5,6 +5,8 @@ import yaml
 from yaml.loader import SafeLoader
 import numpy as np
 import json
+import logging
+from slack_sdk import WebClient
 
 class SlackBot():
     """
@@ -34,19 +36,29 @@ class SlackBot():
 
         """
 
+        client = WebClient(os.environ[self.token])
+
+        new_file = client.files_upload_v2(title="TestFile",filename=impath,content="Placeholder Text")
+
+        file_url = new_file.get("file").get("permalink")
+        return client.chat_postMessage(channel=self.channel,text=f"{file_url}")
+
+        # maybe use the below?
+        # upload_and_then_share_file = client.files_upload_v2(channel="C123456789",title="Test text data",filename="test.txt",content="Hi there! This is a text file!",initial_comment="Here is the file:")
+
         # post = {}
         # post["value1"] = ''
         # post["value2"] = open(impath,'rb').read()
 
         # requests.post(self._link, data=post)
 
-        my_file = {'file' : (impath, open(impath, 'rb'), 'png')}
+        # my_file = {'file' : (impath, open(impath, 'rb'), 'png')}
 
-        payload={"filename":impath,"token":self.token,"channels":self.channel}
+        # payload={"filename":impath,"token":self.token,"channels":self.channel}
 
         # dicto = {'token':self.token,'filename': impath, 'channels': self.channel}
             
-        return requests.post("https://slack.com/api/files.upload", params=payload, files=my_file)
+        # return requests.post("https://slack.com/api/files.upload", params=payload, files=my_file)
 
 
     def post_message(self, subject: str, text: str) -> None:
