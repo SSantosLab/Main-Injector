870a23e3a407a0bde053be1fa1f932f78140785c
Sean MacBride <sean.p.macbride@gmail.com>
2024-03-19 21:24:09 -0700

    Update kn_strategy_sims_MI.py

--- python/knlc/kn_strategy_sims_MI.py
+++ python/knlc/kn_strategy_sims_MI.py
@@ -422,10 +422,10 @@ class KNCalc():
             use_map_info = join(lowres_path,f"{map_name}_mapinfo.npy")
 
             if not os.path.isdir(lowres_path):
-                os.mkdirs(lowres_path,exist_ok=True)
+                os.makedirs(lowres_path,exist_ok=True)
             
             if not os.path.isdir(weights_path):
-                os.mkdirs(weights_path,exist_ok=True)
+                os.makedirs(weights_path,exist_ok=True)
 
             if m_exp_kncalc == False:
                 use_map_weights = join(path2map, "weights"),
