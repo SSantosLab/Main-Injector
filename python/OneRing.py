d094ddbca1aa93c042d6b2690f19408a59b6f4cb
elisekesler <107507728+elisekesler@users.noreply.github.com>
2024-03-29 16:58:25 -0400

    Fixed reverse inputs of get_hexlist in line 105

--- python/OneRing.py
+++ python/OneRing.py
@@ -102,7 +102,7 @@ def run_or(
     
     # From here on is implementation of The Main Injectors (consisting of Elise, Nora, Isaac, Thomas, and Andre) new hex sorting code, with sorting based on awesomeness factor
     og_inner_hexlist, sunrise, sunset = af.get_hexinfo(inner_ra, inner_dec, inner_prob, inner_exptime, filt, mjd, detP,True)
-    og_outer_hexlist = af.get_hexinfo(outer_ra, outer_dec, outer_prob, outer_exptime, filt, detP,mjd)
+    og_outer_hexlist = af.get_hexinfo(outer_ra, outer_dec, outer_prob, outer_exptime, filt, mjd, detP)
 
     # def find_total_sky(self):
         # self.get_hexlists()
